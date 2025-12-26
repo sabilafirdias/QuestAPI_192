04b9b377672c2e52381534d7ce665b30ad9a733a
sabilafirdias <sabilafirdias@gmail.com>
2025-12-26 15:05:39 +0700

    memperbaiki entryviewmodel

--- app/src/main/java/com/example/pertemuan12/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/EntryViewModel.kt
@@ -10,14 +10,12 @@ import com.example.pertemuan12.modeldata.toDataSiswa
 import com.example.pertemuan12.repositori.RepositoryDataSiswa
 import retrofit2.Response
 
-class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
-    ViewModel() {
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
     /* Fungsi untuk memvalidasi input */
-    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ):
-            Boolean {
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
         return with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
@@ -25,17 +23,13 @@ class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
     //Fungsi untuk menangani saat ada perubahan pada text input
     fun updateUiState(detailSiswa: DetailSiswa) {
         uiStateSiswa =
-            UIStateSiswa(
-                detailSiswa = detailSiswa, isEntryValid = validasiInput
-                    (detailSiswa)
-            )
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
 
     /* Fungsi untuk menyimpan data yang di-entry */
     suspend fun addSiswa() {
         if (validasiInput()) {
-            val sip:Response<Void> =repositoryDataSiswa.postDataSiswa(uiStateSiswa
-                .detailSiswa.toDataSiswa())
+            val sip:Response<Void> =repositoryDataSiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
             if (sip.isSuccessful){
                 println("Sukses Tambah Data : ${sip.message()}")
             }else{
